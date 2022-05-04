6d77b7d985fd579b631fcc44c686852382351463
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-05-04 11:03:08 -0400

    removed boiler plate code from App.js, replaced with new code, added p tags to navbar until I make those links, added home folder and home.js and home.css

---
+++ src/Components/navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css'
+
+
+
+const Navbar = () => {
+    return(
+        <div className='Nav'>
+            <p>About</p>
+            <p>Portfolio</p>
+            <p>Contact</p>
+        </div>
+    )
+    };
+
+export default Navbar;
