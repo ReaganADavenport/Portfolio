2526ee9b4f7e656c2fc30705d8f658c0393aa003
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-05-04 14:01:35 -0400

    added card and portfolio components, added porfolio to App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 import Nav from './Components/navbar/navbar';
 import Footer from './Components/footer/footer';
-import Home from './Components/home/home'
+import Home from './Components/home/home';
+import Portfolio from './Components/portfolio/portfolio';
 
 
 
@@ -13,6 +14,7 @@ function App() {
     <div className="Body">
       <Nav></Nav>
       <Home></Home>
+      <Portfolio></Portfolio>
       <Footer></Footer>
     </div>
 )
---
+++ src/Components/portfolio/portfolio.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './portfolio.css';
+
+
+
+const Portfolio = () => {
+    return(
+        <div className="Gallery">
+            <h1>Hi, I'm The Portfolio!</h1>
+        </div>
+    )
+}
+
+export default Portfolio;
