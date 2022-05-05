4f287f805aa2dd2ebc1c14e6b9c6d40a1f69e3ea
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-05-05 16:33:45 -0400

    added logo to navbar + resized it, not totally happy with it

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './navbar.css'
 
-
+import logo from '../../Images/logo.png'
 
 const Navbar = () => {
     return(
         <div className='Nav'>
             <div className='Logo'>
-
+                <img src={logo}></img>
             </div>
             <div className='Links'>
                 <p>About</p>
