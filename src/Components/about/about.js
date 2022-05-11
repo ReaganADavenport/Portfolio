859fead53979559c00d65351fb8566e8b03ba277
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-05-11 19:31:36 -0400

    made the p's into li's

--- src/Components/about/about.js
+++ src/Components/about/about.js
@@ -10,7 +10,7 @@ const About = () => {
                 <h1>About Me</h1>
                 <p>I'm currently looking for work.</p>
                 <p>I'm a curious, skilled, and creative Full-stack Developer with a passion for making beautiful websites. I'm a fast learner, with a strong foundation in Object Oriented Programming, React, and e-commerce.</p>
-                <p>I'm an introvert with excellent communication skills. That combined with my expertise mean that I can explain technical subjects to both laymen and professionals alike.</p>
+                <p>I have excellent communication skills. That combined with my expertise mean that I can explain technical subjects to both laymen and professionals alike.</p>
 
             </div>
             
--- src/Components/mandarin/mandarin.js
+++ src/Components/mandarin/mandarin.js
@@ -11,20 +11,36 @@ const Mandarin = () => {
         <div className="Mandarin">
             <h1>Learn Mandarin</h1>
             <div className='content'>
+                <div className='highlights'>
+                    <h2>Highlights:</h2>
+                    <p>React, API, HTML, CSS, JavaScript</p>
+                </div>
                 <div className='m-block-1'>
                     <h3>Project Goal</h3> 
-                    <p>My aim with this project was to create an application that would allow the user to have flashcards on Mandarin vocabulary, an online space to practice writing out the characters, and finally test yourself on writing characters. I ultimately did reach my goal, but looking back my CSS was not the best, and I would change somethings if I was to reattempted this project.</p>
+                    <p>My aim with this project was: </p>
+                    <ul>
+                        <li>to create an application that would allow the user to have flashcards on Mandarin vocabulary</li>
+                        <li>an online space to practice writing out the characters</li>
+                        <li>finally test yourself on writing characters</li>
+                    </ul>
                 </div>
                 <img className='m-img-1' src={mandarin1}></img>
                 <img className='m-img-2' src={mandarin2}></img>
                 <div className='m-block-2'>
                     <h3>Challenges</h3>
-                    <p>I had inital trouble with getting the flashcards to flip properly, but that was a realtively easy hurdle. The real trouble came when I wanted to compare written Chinese to an image. I needed it to be sensitive enough to pick up when someone wrote the wrong character, but not too sensitive to ding someone who wrote in the correct character.</p>
+                    <ul>
+                        <li>I had inital trouble with getting the flashcards to flip properly, but that was a realtively easy hurdle.</li>
+                        <li>The real trouble came when I wanted to compare written Chinese to an image. I needed it to be sensitive enough to pick up when someone wrote the wrong character, but not too sensitive to ding someone who wrote in the correct character.</li>
+                    </ul>
                 </div>
                 <img className='m-img-3' src={mandarin3}></img>
                 <div className='m-block-3'>
                     <h3>What I Learned</h3>
-                    <p>I learned that I was capable of building a full-stack application with minimal guidance, which I needed since this was one of my earliest projects. Looking back with my experience, I see now that my styles were severly lacking. In the future, I plan to reattempt this project with better UI.</p>
+                    <ul>
+                        <li>I learned that I was capable of building a full-stack application with minimal guidance, which I needed since this was one of my earliest projects.</li>
+                        <li>Knowing what I know now, I took the opportunity to improve my CSS skills.</li>
+                        <li>In the future, I plan to reattempt this project with better UI.</li>
+                    </ul>
                 </div>
                 <div className='m-block-4'>
                     <h4>Please Checkout My Code Here:</h4>
