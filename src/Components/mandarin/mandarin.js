783f39b4278e3286efea0377a888fc7dcc7505ef
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-05-11 13:18:38 -0400

    futzing with the size of the blocks on Mandarin page, adding links to githubs repos, changing the color of the text

--- src/Components/mandarin/mandarin.js
+++ src/Components/mandarin/mandarin.js
@@ -1,7 +1,8 @@
 import React,{Component} from 'react';
 import './mandarin.css';
-import mandarin1 from '../../Images/mandarin1.png'
-import mandarin2 from '../../Images/mandarin2.png'
+import mandarin1 from '../../Images/mandarin1.png';
+import mandarin2 from '../../Images/mandarin2.png';
+import mandarin3 from '../../Images/mandarin3.png';
 
 
 
@@ -20,6 +21,18 @@ const Mandarin = () => {
                     <h3>Challenges</h3>
                     <p>I had inital trouble with getting the flashcards to flip properly, but that was a realtively easy hurdle. The real trouble came when I wanted to compare written Chinese to an image. I needed it to be sensitive enough to pick up when someone wrote the wrong character, but not too sensitive to ding someone who wrote in the correct character.</p>
                 </div>
+                <img className='m-img-3' src={mandarin3}></img>
+                <div className='m-block-3'>
+                    <h3>What I Learned</h3>
+                    <p>I learned that I was capable of building a full-stack application with minimal guidance, which I needed since this was one of my earliest projects. Looking back with my experience, I see now that my styles were severly lacking. In the future, I plan to reattempt this project with better UI.</p>
+                </div>
+                <div className='m-block-4'>
+                    <h4>Please Checkout My Code Here:</h4>
+                    <a href="https://github.com/ReaganADavenport/learn-mandarin-client"><p>GitHub Client-Side</p></a>
+                    <a href="https://github.com/ReaganADavenport/learn-mandarin-api"><p>GitHub API</p></a>
+                </div>
+                
+                
             </div>
             
         </div>
