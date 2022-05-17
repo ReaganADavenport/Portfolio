5baa96252b29f355ae2f105eab5bc386557966d8
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-05-17 10:49:55 -0400

    adding text and links to press page

--- src/Components/press/press.js
+++ src/Components/press/press.js
@@ -19,8 +19,8 @@ const Press = () => {
                     <h3>Project Goal</h3> 
                     <p>My aim with this project was: </p>
                     <ul>
-                        <li></li>
-                       
+                        <li>To create an online home for a book and a direct way to buy the book</li>
+                        <li>To make a referance guide for the "relics" mentioned in the book</li>
                     </ul>
                 </div>
                 <img className='p-img-1' src={press1}></img>
@@ -28,24 +28,25 @@ const Press = () => {
                 <div className='p-block-2'>
                     <h3>Challenges</h3>
                     <ul>
-                        <li></li>
-                        
+                        <li>There was no plan</li>
+                        <li>There was no clear design</li>
                     </ul>
                 </div>
                 <img className='p-img-3' src={press3}></img>
                 <div className='p-block-3'>
                     <h3>What I Learned</h3>
                     <ul>
-                        <li></li>
-    
+                        <li>ALWAYS have a plan or at least a design in mind</li>
+                        <li>Know who this is being aimed towards</li>
+                        <li>How to make a glowing neon sign in CSS</li>
                     </ul>
                 </div>
                 <div className='p-block-4'>
                     <h4>Please Checkout My Code Here:</h4>
-                    <a href="https://github.com/ReaganADavenport/Typescript_Calculator"><p>GitHub Press A to Begin</p></a>
+                    <a href="https://github.com/ReaganADavenport/press-A-to-begin"><p>GitHub Press A to Begin</p></a>
                     
                     <h4>Checkout the Demo Videos:</h4>
-                    <a href='https://youtu.be/vlXVfDkdum4'><p>Press A to Begin Demo</p></a>
+                    <a href='https://www.youtube.com/watch?v=boIwlId1_Zg'><p>Press A to Begin Demo</p></a>
                 </div>
                 
                 
