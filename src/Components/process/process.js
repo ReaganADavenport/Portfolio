0a64d10d87ab831e76fa30d1b20515334f76d2eb
Reagan Davenport <reagan.a.davenport@gmail.com>
2023-10-18 17:23:33 -0400

    made 2 new components, process and resume

---
+++ src/Components/process/process.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './process.css';
+
+
+
+const Process = () => {
+    return(
+        <div className="Process">
+            <h1>Process</h1>
+            
+            
+        </div>
+    )
+}
+
+export default Process;
---
+++ src/Components/resume/resume.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './resume.css';
+
+
+
+const Resume = () => {
+    return(
+        <div className="Resume">
+            <h1>Experience</h1>
+                <h2>Davenport•Klein Commercial Real Estate - Web Developer - Septmeber 2023 - Present</h2>
+                    <ul>
+                        <li>Collaborated closely with the client to capture their vision, brand identity, and unique requirements, resulting in a website that aligns perfectly with their goals and objectives.</li>
+                        <li>Built a brochure website with UI that allowed the employers’s clients is easily find the information they needed</li>
+                    </ul>
+                <h3>What I Learned</h3>
+                    <ul>
+                        <li>Addressing goals and expectaions for the website at the start</li>
+                        <li>Impoved at using AI to create text</li>
+                    </ul>
+                <h2>STARTALK - Web Administrative Assistant - September 2023</h2>
+                    <ul>
+                        <li>Refactored landing page with most recent promotion material</li>
+                        <li>Updated the contact page of the company's website to ensure efficient email distribution, allowing multiple team members to receive inquiries and respond promptly</li>
+                        <li>Reconfigured a design element to be more responsive</li>
+                    </ul>
+                <h3>What I Learned</h3>
+                    <ul>
+                        <li></li>
+                    </ul>
+
+            
+        </div>
+    )
+}
+
+export default Resume;
